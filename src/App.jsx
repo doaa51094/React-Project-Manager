d12c5390f030e64e60b3c825b78f40544f53a808
doaa51094 <112436865+doaa51094@users.noreply.github.com>
2024-08-04 15:27:19 +0300

    create new componenet when no project selected yet

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import NewProject from "./components/NewProject";
+import NoProjectSelected from "./components/NoProjectSelected";
 import ProjectsSidebar from "./components/ProjectsSidebar";
 
 function App() {
   return (
     <main className="my-8 h-screen flex gap-8">
       <ProjectsSidebar/>
-      <NewProject/>
+      {/* <NewProject/> */}
+      <NoProjectSelected/>
     </main>
   );
 }
